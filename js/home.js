539701d797f65f4123619c0db0310d93a88c34d6
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2017-03-12 15:23:49 +0530

    Slider, Announcements, News, Events added

--- js/home.js
+++ js/home.js
@@ -3,32 +3,29 @@ function getPageName(url) {
       var filenameWithExtension = url.substr(index);
       var filename = filenameWithExtension.split(".")[0];
       return filename;
+}
+
+$(document).ready(function() {
+  $("#first").removeClass("active");
+  $("#second").removeClass("active");
+  $("#third").removeClass("active");
+  $("#fourth").removeClass("active");    
+  var pathname = window.location.pathname;
+  var filename = getPageName(pathname);
+  switch (filename) {
+    case "index":
+      $("#first").addClass("active");
+      break;
+    case "gallery":
+      $("#third").addClass("active");
+      break;
   }
-  $(document).ready(function() {
-    $("#first").removeClass("active");
-    $("#second").removeClass("active");
-    $("#third").removeClass("active");
-    $("#fourth").removeClass("active");    
-    var pathname = window.location.pathname;
-    var filename = getPageName(pathname);
-    switch (filename) {
-      case "index":
-        $("#first").addClass("active");
-        break;
-      case "gallery":
-        $("#third").addClass("active");
-        break;
-    }
-  });
+	// $('#header').load('header.html');
+	$('#footer').load('footer.html');
+  $('.slider').slider();;
+});
   
 function autoplay() {
-	$('.carousel').carousel('next');
-	setTimeout(autoplay, 3500);
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 3500);
 };
-$(document).ready(function() {
-	$('#header').load('header.html');
-	$('#footer').load('footer.html');
-	$('.carousel.carousel-slider').carousel({fullWidth: true});
-	setTimeout(autoplay, 2000);
-});
-
